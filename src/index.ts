ccb9431fdb12a307a5a5c3f7b204b09fcd9b5380
Perwollnt <Proedge1@skyreflect.hu>
2022-10-12 13:24:15 +0000

    first commit

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// < Imports >
-import { BotClient } from "./client";
-import { config } from "dotenv";
-
-// Dotenv config
-config();
-
-//Start bot
-new BotClient(process.env.TOKEN!);
--- src/interfaces/Logger.ts
+++
@@ -1,6 +0,0 @@
-export interface LoggerInterface {
-    LEVEL: 0 | 1 | 2 | 3 | 4,
-    TYPE: "SYSTEM" | "EVENT" | "ERROR",
-    MESSAGE: string,
-    SENDER: string
-}
